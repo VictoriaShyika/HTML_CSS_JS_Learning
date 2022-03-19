0291e6fbf8b6ac24c60f66a7ac8ed0c906c5a4a6
VictoriaShyika <victoriashyika@gmail.com>
2022-03-19 20:58:17 +0200

    Synchronized the browser with our data

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -2,13 +2,24 @@ import { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name) {
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   }
 
   const tasksList = tasks.map((task) => (
@@ -17,8 +28,12 @@ function App(props) {
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
+
+  const tasksNoun = tasksList.length !== 1 ? "tasks" : "task";
+  const headingText = `${tasksList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -29,7 +44,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
