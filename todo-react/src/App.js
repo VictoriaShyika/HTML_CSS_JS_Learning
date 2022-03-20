6ec70f4fef8d342ce913c3ee8429fae55e637381
VictoriaShyika <victoriashyika@gmail.com>
2022-03-20 15:04:50 +0200

    Added editing from the UI

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -28,9 +28,9 @@ function App(props) {
   }
 
   function editTask(id, newName) {
-    const editedTaskList = tasks.map((task) => {
-      if ((id = task.id)) {
-        return { ...task, newName };
+    const editedTaskList = tasks.map(task => {
+      if ((id === task.id)) {
+        return { ...task, name: newName };
       }
       return task;
     });
